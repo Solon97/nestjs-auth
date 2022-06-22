aeac96a83f7ecadc4aa89d1ed374cd6ec8b3800e
Solon Barbosa <solonb20@gmail.com>
2022-06-22 15:24:46 +0000

    feat(auth): create files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { PrismaModule } from './prisma/prisma.module'
 import { UserModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, UserModule],
+  imports: [PrismaModule, UserModule, AuthModule],
   controllers: [AppController]
 })
 export class AppModule {}
