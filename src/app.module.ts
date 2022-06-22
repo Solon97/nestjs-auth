4a2297ecc033d606e216c9a57189a94230c46c98
Solon Barbosa <solonb20@gmail.com>
2022-06-22 01:48:01 +0000

    chore: enforce code style

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { PrismaModule } from './prisma/prisma.module'
+import { UserModule } from './user/user.module'
 
 @Module({
   imports: [PrismaModule, UserModule],
-  controllers: [AppController],
+  controllers: [AppController]
 })
 export class AppModule {}
