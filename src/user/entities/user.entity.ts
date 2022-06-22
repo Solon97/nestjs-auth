dbfa8ff4e0ae77bb975d2aed965fb1cd3ffd6e1b
Solon Barbosa <solonb20@gmail.com>
2022-06-22 01:49:14 +0000

    feat(user): create and find user by id services

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,10 @@
-export class User {}
+import { Prisma } from '@prisma/client'
+
+export class User implements Prisma.UserUncheckedCreateInput {
+  id?: number
+  email: string
+  password: string
+  name: string
+  createdAt?: Date | string
+  updatedAt?: Date | string
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
-
+import { Injectable, NotFoundException } from '@nestjs/common'
+import { Prisma } from '@prisma/client'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import bcrypt from 'bcrypt'
+import { PrismaService } from '../prisma/prisma.service'
+import { User } from './entities/user.entity'
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor (private readonly prisma: PrismaService) {}
+
+  async create (createUserDto: CreateUserDto): Promise<User> {
+    const data: Prisma.UserCreateInput = {
+      ...createUserDto,
+      password: await bcrypt.hash(createUserDto.password, 10)
+    }
+    console.log('hash')
+    const createdUser = await this.prisma.user.create({ data })
+
+    return {
+      ...createdUser,
+      password: null
+    }
   }
 
-  findAll() {
-    return `This action returns all user`;
+  findAll () {
+    return 'This action returns all user'
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findById (id: number) {
+    const user = await this.prisma.user.findUnique({ where: { id } })
+    if (!user) {
+      throw new NotFoundException('Usuário não encontrado')
+    }
+    return user
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  findByEmail (email: string) {
+    return this.prisma.user.findUnique({ where: { email } })
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  update (id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`
+  }
+
+  remove (id: number) {
+    return `This action removes a #${id} user`
   }
 }
